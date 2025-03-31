dc35574df75534c6355b55e4e0dc64295a447324
Cris Rodrigues <crisrodrigues@MBP-de-Cris.lan>
2025-03-31 13:43:07 +0200

    deleting commented code

--- Services/ExchangeRateService.cs
+++ Services/ExchangeRateService.cs
@@ -30,54 +30,20 @@ namespace CurrencyExchangeAPI.Services
 
         public async Task<List<ExchangeRate>> FetchAndStoreExchangeRatesAsync()
         {
-            _logger.LogInformation($"vai chamar a API: {ECB_API_URL}");
-            //var response = await _httpClient.GetAsync(ECB_API_URL);
             var response = await _httpClient.GetStringAsync(ECB_API_URL);
-            _logger.LogInformation($"RESPONSE: {response}");
 
             var ratesData = JsonConvert.DeserializeObject<ExchangeRatesResponse>(response);
-             _logger.LogInformation($"RATES DATA: {ratesData}");
 
              if (ratesData?.Result != "success")
             {
                 throw new InvalidOperationException($"API response error: {ratesData?.Result}");
             }
 
-            _logger.LogInformation($"CONVERSION RATES: {ratesData.ConversionRates}");
             if (ratesData?.ConversionRates == null || ratesData.ConversionRates.Count == 0)
             {
                 throw new Exception("No exchange rates available in the response.");
             }
-            //_logger.LogInformation($"Content-Type: {response.Content.Headers.ContentType}");
-
-            //string responseString = await response.Content.ReadAsStringAsync();
-            //_logger.LogInformation($"Raw api response: {responseString}");
             
-            /*if(!response.IsSuccessStatusCode)
-            {
-                throw new Exception("Failed to fetch exchange rates from ECB.");
-            }*/
-
-            //Parse the response as JSON
-            /*var responseContent = await response.Content.ReadAsStringAsync();
-            _logger.LogInformation($"API Response: {response.Content}");
-
-            if(string.IsNullOrEmpty(responseContent))
-            {
-                throw new Exception("No data received from the API.");
-            }*/
-
-            //var ratesData = JsonConvert.DeserializeObject<ExchangeRatesResponse>(responseContent);//await response.Content.ReadFromJsonAsync<EcbRatesResponse>();
-
-            /*if (ratesData?.ConversionRates == null || !ratesData.ConversionRates.Any())
-            {
-                throw new Exception("No exchange rates available in the response.");
-            }
-            if(ratesData == null || !ratesData.Rates.Any())
-            {
-                throw new Exception("No exchange rates received from ECB.");
-            }*/
-
             var exchangeRates = new List<ExchangeRate>();
             
             foreach(var (currency, rate) in ratesData.ConversionRates)
@@ -96,22 +62,11 @@ namespace CurrencyExchangeAPI.Services
                 throw new Exception("No exchange rates to store.");
             }
 
-            /*if(!exchangeRates.Any())
-            {
-                throw new Exception("Exchange rates list is empty.");
-            }*/
 
             await _context.ExchangeRates.AddRangeAsync(exchangeRates);
             await _context.SaveChangesAsync();
 
             return exchangeRates;
         }
-
-        /*public class ExchangeRateApiResponse
-        {
-            public string Result { get; set; }
-            public Dictionary<string, decimal> ConversionRates { get; set; }
-            
-        }*/
     }
 }
