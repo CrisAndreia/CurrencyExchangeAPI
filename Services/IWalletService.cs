62b35ab82f15daaaca5a3f51ab899ac73afd2eac
Cris Rodrigues <crisrodrigues@MBP-de-Cris.lan>
2025-04-03 18:00:15 +0200

    deleting unused code

--- Services/IWalletService.cs
+++ Services/IWalletService.cs
@@ -8,11 +8,6 @@ namespace CurrencyExchangeAPI.Services
     {
         Task<Wallet> CreateWalletAsync(WalletDto walletCreateDTO);
         Task<Wallet> GetWalletByIdAsync(int id);
-        //Task<WalletBalance> AddBalanceToWalletAsync(int walletId, WalletBalanceDto balanceDto);
-        //Task<WalletBalance> GetBalanceByCurrencyAsync(int walletId, string currency);
-        //Task<WalletBalance> UpdateWalletBalanceAsync(int walletId, WalletBalanceDto balanceDto);
-        //Task<WalletBalance> DepositAsync(int walletId, WalletBalanceDto depositDto);
-        //Task<WalletBalance> WithdrawAsync(int walletId, WalletBalanceDto withdrawDto);
 
     }
 }
